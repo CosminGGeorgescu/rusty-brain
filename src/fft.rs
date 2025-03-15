1320ebe4697f5285cda3125b4df2e4a9a9466779
Cosmin-Gabriel Georgescu <cosmingg2013@gmail.com>
2025-03-15 23:29:33 +0200

    Inverse DFT

--- src/fft.rs
+++ src/fft.rs
@@ -7,16 +7,20 @@ use ndarray::{Array1, Array2};
 use num_traits::identities::Zero;
 use std::f32::consts::PI;
 
-pub trait FourierTransform<S>
-where
-    S: Data<Elem = f32>,
-{
+pub trait FourierTransform {
+    // Naive DFT algorithm
     fn dft(&self) -> Array1<Complex<f32>>;
+    // Cooley-Tukey radix-2 FFT algorithm
     fn fft(&self) -> Array1<Complex<f32>>;
+    // Short-time FT implementation using a sine window
     fn stft(&self, window_size: usize, hop_size: usize) -> Array2<f32>;
 }
 
-impl<S> FourierTransform<S> for ArrayBase<S, Ix1>
+pub trait InverseFourierTransform {
+    fn idft(&self) -> Array1<f32>;
+}
+
+impl<S> FourierTransform for ArrayBase<S, Ix1>
 where
     S: Data<Elem = f32>,
 {
@@ -28,7 +32,7 @@ where
             let mut sum = Complex::zero();
 
             for t in 0..n {
-                let angle = -2.0f32 * std::f32::consts::PI * k as f32 * t as f32 / n as f32;
+                let angle = -2.0f32 * PI * k as f32 * t as f32 / n as f32;
                 let twiddle = Complex::new(angle.cos(), angle.sin());
 
                 sum += twiddle * self[t];
@@ -55,7 +59,7 @@ where
 
         let mut result = Array1::zeros(n);
         for k in 0..n / 2 {
-            let angle = -2.0 * std::f32::consts::PI * k as f32 / n as f32;
+            let angle = -2.0 * PI * k as f32 / n as f32;
             let twiddle = Complex::new(angle.cos(), angle.sin());
 
             result[k] = fft_even[k] + twiddle * fft_odd[k];
@@ -66,17 +70,20 @@ where
     }
 
     fn stft(&self, window_size: usize, hop_size: usize) -> Array2<f32> {
+        let next_pow_2 = window_size.next_power_of_two();
         let window = Array1::from_iter(
             (0..window_size).map(|n| (PI * (n as f32 + 0.5) / window_size as f32).sin()),
         );
         let num_frames = (self.len() - window_size) / hop_size + 1;
-        let mut result = Array2::<f32>::zeros((num_frames, window_size));
+        let mut result = Array2::<f32>::zeros((num_frames, next_pow_2));
 
         for i in 0..num_frames {
             let start = i * hop_size;
-            let frame =
-                &Array1::from(self.slice(s![start..start + window_size]).to_owned()) * &window;
-            let spectrum = frame.view().fft();
+            let mut frame = Array1::zeros(next_pow_2);
+            frame
+                .slice_mut(s![..window_size])
+                .assign(&(&self.slice(s![start..start + window_size]) * &window));
+            let spectrum = frame.fft();
 
             result
                 .slice_mut(s![i, ..])
@@ -84,5 +91,30 @@ where
         }
 
         result
+    }
+}
+
+impl<S> InverseFourierTransform for ArrayBase<S, Ix1>
+where
+    S: Data<Elem = Complex<f32>>,
+{
+    fn idft(&self) -> Array1<f32> {
+        let n = self.len();
+        let mut result = Array1::zeros(n);
+
+        for k in 0..n {
+            let mut sum = 0.0f32;
+
+            for t in 0..n {
+                let angle = 2.0f32 * PI * k as f32 * t as f32 / n as f32;
+                let twiddle = Complex::new(angle.cos(), angle.sin());
+
+                sum += (twiddle * self[t]).re;
+            }
+
+            result[k] = sum;
+        }
+
+        result / n as f32
     }
 }
